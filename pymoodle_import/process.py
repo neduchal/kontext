e95e1d8701be3714cd870c135fec2b9a67b99d60
Petr Neduchal <neduchal@gmail.com>
2022-06-14 10:48:20 +0200

    Dodrzeni AIKEN formatu a pridani prepinace pro dokumenty bez otazky typu shortanswer

--- pymoodle_import/process.py
+++ pymoodle_import/process.py
@@ -4,12 +4,13 @@ import os
 import os.path
 import string
 import glob 
+import sys
 
 input_dir = "input"
 output_dir = "./output"
 
 
-def translate(filename):
+def translate(filename, without_short=False):
     print(filename)
     document =  Document(filename)
 
@@ -19,11 +20,15 @@ def translate(filename):
     completed_text = []
     questions = []
     state = -1
+    second_task_text = ""
+    second_task_words = [] 
 
     for par in document.paragraphs:
         for run_idx, r in enumerate(par.runs):
             if (r.bold == True):
                 state = state + 1
+                if without_short and state == 2:
+                    state = state + 1
             if (state == 0):
                 if  (par.text[0:6] != "source"):
                     cloze_text.append(par.text)
@@ -41,6 +46,12 @@ def translate(filename):
 
     first_task_text = first_task[0]
     first_task_options = first_task[1:-1]
+    for i in range(len(first_task_options)-1, 0, -1):
+        print(i)
+        if first_task_options[i] == "" or first_task_options[i].find("ANSWERS") != -1:
+            first_task_options.pop(i)
+
+
     first_task_answers = []    
     for t in first_task[1:]:
         if t.find("ANSWERS:") != -1:
@@ -59,16 +70,17 @@ def translate(filename):
     for i in range(len(first_task_answers)):
         first_task_answers[i] = alphabet.index(first_task_answers[i][1])
 
-    second_task_text = second_task[0]
-    second_task_words = []
-    for t in second_task[1:]:
-        if t.find("ANSWERS:") != -1:
-            second_task_words = t.split(":")[1].replace(",", "").split()
-    second_task_answers = []
-    for word in second_task_words:
-        second_task_answers.append(word[1])
-        second_task_answers.append(word.split(")")[1])
-        second_task_answers.append(word.replace("(","").replace(")",""))
+    if without_short == False:
+        second_task_text = second_task[0]
+        second_task_words = []
+        for t in second_task[1:]:
+            if t.find("ANSWERS:") != -1:
+                second_task_words = t.split(":")[1].replace(",", "").split()
+        second_task_answers = []
+        for word in second_task_words:
+            second_task_answers.append(word[1])
+            second_task_answers.append(word.split(")")[1])
+            second_task_answers.append(word.replace("(","").replace(")",""))
 
     # MULTICHOICE
     for q in range(5):
@@ -81,7 +93,7 @@ def translate(filename):
                     cloze_text[i] = cloze_text[i][0:index+2] + "_" +  cloze_text[i][index+3:]
             if  (index != -1):
                 index2 = index +2
-                for j in range(12):
+                for j in range(25):
                     if  cloze_text[i][index2] == "_":
                         index2 = index2+1
                     else:
@@ -103,28 +115,33 @@ def translate(filename):
                 if cloze_text[i][index-1] == "(":              
                     cloze_text[i] = cloze_text[i][0:index-1] +  cloze_text[i][index:]         
     #WORDS
-    for w_i in range(0, len(second_task_answers), 3):
-        for i in range(len(cloze_text)):
-            index = cloze_text[i].find(second_task_answers[w_i] + "_") 
-            if  (index != -1):
-                index2 = index +2
-                for j in range(12):
-                    if  cloze_text[i][index2] == "_":
-                        index2 = index2+1
-                    else:
-                        break
-                cloze_text[i] = cloze_text[i].replace(second_task_answers[w_i] + "_" + (index2 - (index +2))*"_", second_task_answers[w_i]) 
-                index = index + 1 
-                answer_str = "{3:SHORTANSWER:%100%"+second_task_answers[w_i + 1]+"#~%100%"+second_task_answers[w_i + 2]+"#}"
-                cloze_text[i] = cloze_text[i][0:index] + answer_str + cloze_text[i][index:]
+    if without_short == False:
+        for w_i in range(0, len(second_task_answers), 3):
+            for i in range(len(cloze_text)):
+                index = cloze_text[i].find(second_task_answers[w_i] + "_") 
+                if  (index != -1):
+                    index2 = index +2
+                    for j in range(12):
+                        if  cloze_text[i][index2] == "_":
+                            index2 = index2+1
+                        else:
+                            break
+                    cloze_text[i] = cloze_text[i].replace(second_task_answers[w_i] + "_" + (index2 - (index +2))*"_", second_task_answers[w_i]) 
+                    index = index + 1 
+                    answer_str = "{3:SHORTANSWER:%100%"+second_task_answers[w_i + 1]+"#~%100%"+second_task_answers[w_i + 2]+"#}"
+                    cloze_text[i] = cloze_text[i][0:index] + answer_str + cloze_text[i][index:]
 
     return cloze_text, first_task_text, first_task_options, first_task_answers, second_task_text, second_task_words, completed_text, questions
 
 if __name__ == "__main__":
+    without_short = False
+    if len(sys.argv) > 1 and sys.argv[1] == "true":
+        without_short = True
     files = glob.glob(os.path.join(input_dir, "*.docx"))
     alphabet = "ABCDEF"
     for f in files:
-        cloze_text, first_task_text, first_task_options, first_task_answers, second_task_text, second_task_words, completed_text, questions = translate(f)
+        cloze_text, first_task_text, first_task_options, first_task_answers, second_task_text, second_task_words, completed_text, questions = translate(f, without_short)
+
         output_filename = os.path.basename(f).split(".")[0] + ".txt"
         of = open(os.path.join(output_dir, output_filename), "w")
         for row in cloze_text:
@@ -133,29 +150,40 @@ if __name__ == "__main__":
         of.write(first_task_text  + "\n")
         for option in first_task_options:
             of.write(option  + "\n")
-        of.write("ANSWERS: ")            
+        of.write("ANSWERS: ")          
+        print(first_task_options)  
         for i, answer in enumerate(first_task_answers):
             of.write(str(i+1)  +  alphabet[answer])
             if i < len(first_task_answers):
                 of.write(", ")
         of.write("\n\n" + 80*"#" + "\n")
-        of.write(second_task_text  + "\n") 
-        of.write("ANSWERS: ")          
-        for i, answer in enumerate(second_task_words):
-            of.write(answer)
-            if i < len(second_task_words):
-                of.write(", ")
-        of.write("\n\n" + 80*"#" + "\n")      
+        if without_short == False:
+            of.write(second_task_text  + "\n") 
+            of.write("ANSWERS: ")          
+            for i, answer in enumerate(second_task_words):
+                of.write(answer)
+                if i < len(second_task_words):
+                    of.write(", ")
+            of.write("\n\n" + 80*"#" + "\n")      
         for i, row in enumerate(completed_text):
             if (i > 0):
                 of.write(row + "\n\n")      
             else:
                 of.write(row + "\n")         
         of.write("\n\n" + 80*"#" + "\n")                           
+        index = -1
+        a = ["A. ", "B. ", "C. ", "D. ", "E. "]
         for row in questions:
-            of.write(row + "\n")    
             if row.find("ANSWER:") != -1:
+                index = -1
+                of.write(row + "\n") 
                 of.write("\n")
+                continue
+            if index >=0:
+                of.write(a[index] + row + "\n")
+            else:
+                of.write(row + "\n") 
+            index += 1
         of.close()
         
 
